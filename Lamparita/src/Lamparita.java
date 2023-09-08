06a1a2ebdc9b1fdc19e8c036a57ba6180a07768e
Cacho <niko.kane@gmail.com>
2023-09-08 17:45:16 -0300

    add methodds

--- Lamparita/src/Lamparita.java
+++ Lamparita/src/Lamparita.java
@@ -1,4 +1,19 @@
 public class Lamparita {
+
+    private boolean isOn = false;
+
+    public void turnOn() {
+        this.isOn = true;
+    }
+
+    public void turnOff() {
+        this.isOn = false;
+    }
+
+    public boolean estado() {
+        return this.isOn;
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Vamos a programar una lamparita! ");
     }
